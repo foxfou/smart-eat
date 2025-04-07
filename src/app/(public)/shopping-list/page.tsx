05ced916ef2af3b8400bc311cfa21e97fc4686d4
Александр Филипенко <sasha@3davinci.ru>
2025-04-08 01:54:15 +0300

    fix: base path

--- src/app/(public)/shopping-list/page.tsx
+++ src/app/(public)/shopping-list/page.tsx
@@ -5,7 +5,7 @@ export default function ShoppingListPage() {
     const [list, setList] = useState<ShoppingList | null>(null)
 
     useEffect(() => {
-        fetch(`/shopping-list.json`)
+        fetch(`./shopping-list.json`)
             .then((res) => res.json())
             .then(setList)
     }, [])
