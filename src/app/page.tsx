b7d189ec02ca65481a1da4720914510eacf717b5
Александр Филипенко <sasha@3davinci.ru>
2025-04-08 01:29:50 +0300

    fix: base path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 
 // pages/index.tsx
 export default function Home() {
-    console.log('123 ',process.env.BASE_PATH)
+
     return (
         <main className="p-6">
             <h1 className="text-2xl font-bold mb-4">LifeMenu</h1>
             <ul className="list-disc list-inside">
+                process.env.BASE_PATH: {process.env.BASE_PATH}
                 <li><a href={`${process.env.BASE_PATH}/menu`}>Меню на неделю</a></li>
                 <li><a href={`${process.env.BASE_PATH}/shopping-list`}>Список покупок</a></li>
             </ul>
